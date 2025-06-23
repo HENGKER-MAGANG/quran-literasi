7595ea85f24513da1e5869a94ba6890fe160712e
Ikhsan <pratamaikhsan974.com>
2025-06-23 18:53:31 +0800

    configurasi db

--- config/db.php
+++ config/db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
+$host = "nw48koo4g04swkkwkw0400sg"; // Ganti sesuai host internal lengkap dari Coolify
 $dbname = "alquran_literasi";
-$user = "root";
-$pass = "";
+$user = "quran";
+$pass = "quran123";
+$port = 3306;
 
 try {
-  $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+  $db = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("Koneksi gagal: " . $e->getMessage());
